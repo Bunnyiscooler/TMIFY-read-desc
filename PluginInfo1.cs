65bbd7832ee6c407a5e943d714e82d21dac77489
Bunnyiscooler <prod.y2kbuns@gmail.com>
2026-02-08 16:49:38 -0500

    Create PluginInfo1.cs

---
+++ PluginInfo1.cs
@@ -0,0 +1,12 @@
+namespace PeformMore
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.bunnyzxxx.gorillatag.PerformMore";
+        public const string Name = "PerformMore";
+        public const string Version = "1.0.0";
+    }
+}
