533a5ab9d17b5af64b7960bf4f97bff8dc370a71
Bunnyiscooler <prod.y2kbuns@gmail.com>
2026-03-26 03:31:44 -0400

    Update GUID and version in PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,9 +5,9 @@
     /// </summary>
     internal class PluginInfo
     {
-        public const string GUID = "com.bunnyzxxx.gorillatag.TMIFY";
+        public const string GUID = "com.Bunnyzxxx.TMIFY";
         public const string Name = "TMIFY";
-        public const string Version = "7.6.7";
+        public const string Version = "0.6.7";
         public const string ClientResourcePath = "LibrePad.Resources";
     }
 }
